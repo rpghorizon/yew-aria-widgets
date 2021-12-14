8478deaf708e2d60189a8603ea32b6bea12d59f0
Lucille L. Blumire <lucy@llblumire.co.uk>
2021-12-14 22:19:57 +0000

    Initial crate setup

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+//! Yew Aria Widgets provides components for use within yew.rs applications
+//! that natively support the recommended ARIA and other assistive technology
+//! behaviours, to maximise site accessibility.
+//!
+//! You can read more about these widgets at 
+//! <https://www.w3.org/TR/wai-aria-practices-1.1>
